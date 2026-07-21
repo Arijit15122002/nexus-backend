de6533103bb975c4dbd944547e5495c18403df56
Arijit15122002 <132765143+Arijit15122002@users.noreply.github.com>
2026-07-21 09:42:06 +0530

    Health-check API added

--- src/main/java/com/arijit/nexus_backend/auth/controller/AuthController.java
+++ src/main/java/com/arijit/nexus_backend/auth/controller/AuthController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -39,6 +36,11 @@ public class AuthController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         }
+    }
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
     }
 
 }
